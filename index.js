74683241121cb21f29e373877443d400f6429089
ahmedjs78 <ahmedjs778@gmail.com>
2024-02-16 23:50:20 +0400

    now you can save the input in the backend

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let inputEl = document.getElementBuyId("input-el")
-let inputBtn = document.getElementBuyId("input-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn");
 
 function saveLead( ){
-    console.log("Button clickd!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 }
+
+inputBtn.addEventListener("click",saveLead)
