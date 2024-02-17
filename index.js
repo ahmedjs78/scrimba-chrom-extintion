20b33f3417d98b23f4a888956c11807b0303e447
ahmedjs78 <ahmedjs778@gmail.com>
2024-02-17 22:13:43 +0400

    L

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-let myLeads = [];
+let myLeads = ["bruh",123,2,"qwe21"];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
-
+const ulEl = document.getElementById("ul-el")
 function saveLead( ){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     console.log(myLeads)
+    renderLeads()
 }
 
 inputBtn.addEventListener("click",saveLead)
+
+function renderLeads(){
+
+    let listItem = ""
+    for(let i = 0; i < myLeads.length; i++){
+        
+        listItem +=  `<li>
+                        <a href='${myLeads[i]}' target='_blank' > ${myLeads[i]}
+                        </a>  
+                      </li>`
+    }
+    console.log(listItem)
+    ulEl.innerHTML = listItem
+}
