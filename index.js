cd809326a42306624a3cee1b67364c81675525d6
ahmedjs78 <ahmedjs778@gmail.com>
2024-02-18 22:31:28 +0400

    working on adding localstorage function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-let myLeads = ["bruh",123,2,"qwe21"];
-const inputEl = document.getElementById("input-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el");
+
+
+
 function saveLead( ){
     myLeads.push(inputEl.value)
+    let skrt = []
+    skrt += inputEl.value
+    localStorage.setItem("myLeads", skrt)
     inputEl.value = ""
     console.log(myLeads)
     renderLeads()
+    console.log(localStorage.getItem("myLeads"));
 }
 
 inputBtn.addEventListener("click",saveLead)
@@ -16,10 +23,13 @@ function renderLeads(){
     let listItem = ""
     for(let i = 0; i < myLeads.length; i++){
         
-        listItem +=  `<li>
-                        <a href='${myLeads[i]}' target='_blank' > ${myLeads[i]}
+        listItem +=  `
+                      <li>
+                        <a href='${myLeads[i]}' target='_blank' > 
+                        ${myLeads[i]}
                         </a>  
-                      </li>`
+                      </li>
+                      `
     }
     console.log(listItem)
     ulEl.innerHTML = listItem
