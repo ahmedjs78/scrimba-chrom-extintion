22cfea85a4bfeef626087efc625abec55b752d62
ahmedjs78 <ahmedjs778@gmail.com>
2024-02-21 13:37:02 +0400

    done

--- index.js
+++ index.js
@@ -3,53 +3,67 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn")
+const saveTabBtn = document.getElementById("tab-btn")
+
+
+saveTabBtn.addEventListener('click',saveTab)
+deleteBtn.addEventListener("dblclick",deleteAll)
+inputEl.addEventListener("keydown",saveLead)
+inputBtn.addEventListener("click",saveLead)
+
 
 let leadsfromLocalstorage = JSON.parse(localStorage.getItem("myLeads"))
 if(leadsfromLocalstorage){
     myLeads = leadsfromLocalstorage
-    renderLeads()
+    render(myLeads)
 }
 
-deleteBtn.addEventListener("dblclick",deleteAll)
-function deleteAll(){
-    console.log("deleted")
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-}
 
-//save Leads function
-function saveLead(event){
-    if (event.key === 'Enter' || event.type === "click" ){
 
-        myLeads.push(inputEl.value)
-        
+
+function saveTab(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        
-        leadsfromLocalstorage = JSON.parse(localStorage.getItem("myLeads"))
-        
-        inputEl.value = ""
-        
-        console.log(myLeads)
-        
-        renderLeads()
+        render(myLeads)
+       })
     }
-}
-inputEl.addEventListener("keydown",saveLead)
-inputBtn.addEventListener("click",saveLead)
 
-function renderLeads(){
-    
+
+
+    function render(leads){
+
     let listItem = ""
-    for(let i = 0; i < myLeads.length; i++){
+    for(let i = 0; i < leads.length; i++){
         listItem +=  `
                       <li>
-                        <a href='${myLeads[i]}' target='_blank' >
-                        ${myLeads[i]}
+                        <a href='${leads[i]}' target='_blank' >
+                        ${leads[i]}
                         </a>
                       </li>
                       `
     }
     
     ulEl.innerHTML = listItem
+}
+
+function deleteAll(){
+    console.log("deleted")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+}
+
+//save Leads function
+function saveLead(event){
+    if (event.key === 'Enter' || event.type === "click" ){
+        
+        myLeads.push(inputEl.value)
+        
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        
+        inputEl.value = ""
+        
+        render(myLeads)
+    }
 }
